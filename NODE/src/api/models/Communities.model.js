cc6b010aa16f22564c363323ba7faadd546a37ab
Astrid-VW <astridvanwelden@hotmail.com>
2024-02-11 16:08:11 +0100

    modelosCommunitiesYMessage

---
+++ NODE/src/api/models/Communities.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const communitiesSchema = new Schema( {
+    name: { 
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    type: {
+      type: String,
+      enum: ["private", "public"],
+      required: true,
+    },
+    location: {
+        type: Number, 
+        required: true, 
+        unique: true
+    },
+    description: {
+      type: String,
+      required: true,
+      //ver cómo podemos meter un máx y un mín de caracteres en la descripción.//
+    },
+    users: [
+        { type: mongoose.Schema.Types.ObjectId, 
+            ref: "User" }
+    ]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Communities", communitiesSchema);
+
+module.exports = Communities;
---
+++ NODE/src/api/models/Message.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const MessageSchema = new Schema(
+  {
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    type: {
+      type: String,
+      enum: ["private", "public"],
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    recipientUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    recipientService: { type: mongoose.Schema.Types.ObjectId, ref: "Service" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", MessageSchema);
+
+module.exports = Message;
